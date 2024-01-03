8f7171bb242334a9d1fab8ef2bd1a42dbd66ecec
Renato Silva <renatosilvacode@gmail.com>
2024-01-02 22:07:35 -0300

    feat: add generic pages on sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,12 +3,13 @@ import { createClient } from '~/libs/prismicio'
 export default async function sitemap() {
 	const client = createClient()
 
-	const [authorPages, articlePages, blogPage, homePage] =
+	const [authorPages, articlePages, blogPage, homePage, pages] =
 		await Promise.allSettled([
 			client.getAllByType('author'),
 			client.getAllByType('article'),
 			client.getSingle('blog'),
-			client.getSingle('home')
+			client.getSingle('home'),
+			client.getAllByType('page')
 		])
 
 	const sitemaps = []
@@ -36,6 +37,22 @@ export default async function sitemap() {
 			.filter((document) => document.data.robots_index)
 			.map((document) => ({
 				url: `https://${process.env.HOST}/blog/${document.uid}`,
+				lastModified: document.last_publication_date,
+				changeFrequency: 'weekly',
+				priority: 0.9
+			}))
+
+		sitemaps.push(...articlesSitemap)
+	}
+
+	if (pages.status === 'fulfilled') {
+		const articlesSitemap = pages.value
+			.filter((document) => document.data.robots_index)
+			.map((document) => ({
+				url: `https://${process.env.HOST}/${document.uid.replaceAll(
+					'--',
+					'/'
+				)}`,
 				lastModified: document.last_publication_date,
 				changeFrequency: 'weekly',
 				priority: 0.9
