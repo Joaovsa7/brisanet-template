21dc33ae03595b9f3715059684192a738ca0e95c
Renato Silva <renatosilvacode@gmail.com>
2024-01-06 11:42:06 -0300

    refactor: adjust article card component styles

--- src/components/article-card.tsx
+++ src/components/article-card.tsx
@@ -7,19 +7,19 @@ export function ArticleCard({
 	article
 }: { article: IArticleDocumentResponse }) {
 	return (
-		<article className="flex flex-col shadow-md overflow-hidden rounded-md transition-transform duration-300 hover:shadow-xl hover:-translate-y-1">
+		<article className="group flex flex-col overflow-hidden">
 			<div className="aspect-video">
 				<PrismicNextImage
 					field={article.data.image}
-					className="aspect-video object-cover"
 					width={600}
 					height={337.5}
 					quality={80}
+					className="aspect-video object-cover group-hover:scale-105 transition-transform duration-300"
 				/>
 			</div>
 
-			<div className="flex-1 flex flex-col p-4">
-				<div className="flex items-center justify-between text-sm mb-4">
+			<div className="flex-1 flex flex-col py-3">
+				<div className="flex items-center justify-between text-sm mb-2 uppercase font-medium text-neutral-500">
 					{isFilled.contentRelationship(article.data.category) &&
 						isFilled.richText(article.data.category.data?.name) && (
 							<span>{asText(article.data.category.data.name)}</span>
@@ -39,7 +39,7 @@ export function ArticleCard({
 					</time>
 				</div>
 
-				<h2 className="flex-1 font-semibold transition-colors">
+				<h2 className="flex-1 font-bold transition-colors group-hover:text-primary-500">
 					{asText(article.data.title)}
 				</h2>
 			</div>
