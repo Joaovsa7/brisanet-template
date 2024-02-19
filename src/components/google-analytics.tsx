b0b85c5c702f9555ebfff0bacc6f67f447beeb5e
Renato Silva <renatosilvacode@gmail.com>
2024-02-19 15:21:17 -0300

    build: add @next/third-parties dependency

--- src/components/google-analytics.tsx
+++ src/components/google-analytics.tsx
@@ -1,30 +1,9 @@
-import Script from 'next/script'
+import { GoogleAnalytics as NextGoogleAnalytics } from '@next/third-parties/google'
 
 import { env } from '~/config/env'
 
-const GOOGLE_ANALYTICS_ID = env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
+const GOOGLE_ANALYTICS_ID = env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID as string
 
 export function GoogleAnalytics() {
-	return (
-		<>
-			<Script
-				id={GOOGLE_ANALYTICS_ID}
-				async
-				src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ANALYTICS_ID}`}
-			/>
-			<Script
-				id="google-analytics"
-				// biome-ignore lint/security/noDangerouslySetInnerHtml: <explanation>
-				dangerouslySetInnerHTML={{
-					__html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', '${GOOGLE_ANALYTICS_ID}');
-          `
-				}}
-			/>
-		</>
-	)
+	return <NextGoogleAnalytics gaId={GOOGLE_ANALYTICS_ID} />
 }
