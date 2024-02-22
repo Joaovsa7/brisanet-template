c712b13ce6d0e099a453f6ba5fc15adf3a2a9b35
Renato Silva <renatosilvacode@gmail.com>
2024-02-22 19:41:56 -0300

    fix: hyperlink serializer

--- src/components/rich-text.tsx
+++ src/components/rich-text.tsx
@@ -1,3 +1,4 @@
+import { RTLinkNode } from '@prismicio/client'
 import { PrismicRichText } from '@prismicio/react'
 import type { JSXMapSerializer, PrismicRichTextProps } from '@prismicio/react'
 
@@ -6,33 +7,46 @@ import { slugify } from '~/utils/slugify'
 import { Text } from './text'
 
 interface IRichTextProps extends PrismicRichTextProps {
-  className?: string
+	className?: string
+}
+
+type NodeHyperlinkData = RTLinkNode['data'] & {
+	target: '_blank' | '_self'
 }
 
 const components: JSXMapSerializer = {
-  heading1: ({ children, text }) => <h1 id={slugify(text)}>{children}</h1>,
-  heading2: ({ children, text }) => <h2 id={slugify(text)}>{children}</h2>,
-  heading3: ({ children, text }) => <h3 id={slugify(text)}>{children}</h3>,
-  heading4: ({ children, text }) => <h4 id={slugify(text)}>{children}</h4>,
-  heading5: ({ children, text }) => <h5 id={slugify(text)}>{children}</h5>,
-  heading6: ({ children, text }) => <h6 id={slugify(text)}>{children}</h6>,
-  hyperlink: ({ children, node }) => {
-    return (
-      <a
-        href={node.data?.url?.replaceAll('--', '/')}
-        target={node.data?.target}
-        rel={node.data?.target === '_blank' ? 'noopener noreferrer' : undefined}
-      >
-        {children}
-      </a>
-    )
-  }
+	heading1: ({ children, text }) => <h1 id={slugify(text)}>{children}</h1>,
+	heading2: ({ children, text }) => <h2 id={slugify(text)}>{children}</h2>,
+	heading3: ({ children, text }) => <h3 id={slugify(text)}>{children}</h3>,
+	heading4: ({ children, text }) => <h4 id={slugify(text)}>{children}</h4>,
+	heading5: ({ children, text }) => <h5 id={slugify(text)}>{children}</h5>,
+	heading6: ({ children, text }) => <h6 id={slugify(text)}>{children}</h6>,
+
+	hyperlink: ({ children, node }) => {
+		const url = node.data.url
+		const href = url ? url.replaceAll('--', '/') : ''
+
+		const nodeData = node.data as unknown as NodeHyperlinkData
+		const target = nodeData.target ?? '_self'
+
+		return (
+			<a
+				href={href}
+				target={target}
+				{...(target === '_blank' && {
+					rel: 'noopener'
+				})}
+			>
+				{children}
+			</a>
+		)
+	}
 }
 
 export function RichText({ className, ...props }: IRichTextProps) {
-  return (
-    <Text className={className}>
-      <PrismicRichText {...props} components={components} />
-    </Text>
-  )
+	return (
+		<Text className={className}>
+			<PrismicRichText {...props} components={components} />
+		</Text>
+	)
 }
