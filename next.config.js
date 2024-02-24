183fc527a2981bdd8c68aded269791c4ac578ef6
Renato Silva <renatosilvacode@gmail.com>
2024-02-24 11:09:09 -0300

    build: add /telefone path on redirect list

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: '/telefone',
+				destination: '/numero-da-brisanet',
+				permanent: true
+			}
+		]
+	}
+}
 
 module.exports = nextConfig
