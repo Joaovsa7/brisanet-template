0be3a6196a4d810856be370e1ad47d7bcb819cd4
Renato Silva <renatosilvacode@gmail.com>
2024-03-12 22:19:25 -0300

    refactor: create root layout to pages

---
+++ src/app/(root)/layout.tsx
@@ -0,0 +1,56 @@
+import { createClient } from '~/libs/prismicio'
+
+import { env } from '~/config/env'
+
+import { ConversionBar } from '~/components/conversion-bar'
+import { Footer } from '~/components/footer'
+import { GoogleAnalytics } from '~/components/google-analytics'
+import { Header } from '~/components/header'
+
+export async function generateMetadata() {
+	const siteName = env.NEXT_PUBLIC_SITE_NAME
+
+	return {
+		title: {
+			default: siteName,
+			template: `%s | ${siteName}`
+		},
+		description: 'Encontre a melhor operadora para o seu perfil',
+		site_name: siteName,
+		locale: 'pt_BR',
+		type: 'website',
+		robots: {
+			googleBot: {
+				'max-video-preview': -1,
+				'max-image-preview': 'large',
+				'max-snippet': -1
+			}
+		},
+		openGraph: {
+			siteName,
+			locale: 'pt_BR',
+			type: 'website'
+		}
+	}
+}
+
+export default async function Layout({
+	children
+}: {
+	children: React.ReactNode
+}) {
+	const client = createClient()
+
+	const headerDocument = await client.getSingle('header').catch(() => null)
+	const footerDocument = await client.getSingle('footer').catch(() => null)
+
+	return (
+		<div className="bg-neutral-100 text-neutral-800 flex flex-col min-h-screen overflow-x-hidden">
+			<GoogleAnalytics />
+			<Header headerDocument={headerDocument} />
+			<div className="flex flex-col flex-1">{children}</div>
+			<ConversionBar />
+			<Footer footerDocument={footerDocument} />
+		</div>
+	)
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,6 @@
 import { Barlow } from 'next/font/google'
 import './globals.css'
 
-import { createClient } from '~/libs/prismicio'
-
-import { Footer } from '~/components/footer'
-import { GoogleAnalytics } from '~/components/google-analytics'
-import { Header } from '~/components/header'
-
-import { ConversionBar } from '~/components/conversion-bar'
-import { env } from '~/config/env'
-
 const font = Barlow({
 	weight: ['400', '500', '600', '700'],
 	display: 'swap',
@@ -17,52 +8,14 @@ const font = Barlow({
 	subsets: ['latin']
 })
 
-export async function generateMetadata() {
-	const siteName = env.NEXT_PUBLIC_SITE_NAME
-
-	return {
-		title: {
-			default: siteName,
-			template: `%s | ${siteName}`
-		},
-		description: 'Encontre a melhor operadora para o seu perfil',
-		site_name: siteName,
-		locale: 'pt_BR',
-		type: 'website',
-		robots: {
-			googleBot: {
-				'max-video-preview': -1,
-				'max-image-preview': 'large',
-				'max-snippet': -1
-			}
-		},
-		openGraph: {
-			siteName,
-			locale: 'pt_BR',
-			type: 'website'
-		}
-	}
-}
-
-export default async function RootLayout({
+export default function RootLayout({
 	children
 }: {
 	children: React.ReactNode
 }) {
-	const client = createClient()
-
-	const headerDocument = await client.getSingle('header').catch(() => null)
-	const footerDocument = await client.getSingle('footer').catch(() => null)
-
 	return (
 		<html lang="pt-BR" className={font.variable}>
-			<body className="antialiased bg-neutral-100 text-neutral-800 flex flex-col min-h-screen overflow-x-hidden">
-				<GoogleAnalytics />
-				<Header headerDocument={headerDocument} />
-				<div className="flex flex-col flex-1">{children}</div>
-				<ConversionBar />
-				<Footer footerDocument={footerDocument} />
-			</body>
+			<body className="antialiased">{children}</body>
 		</html>
 	)
 }
