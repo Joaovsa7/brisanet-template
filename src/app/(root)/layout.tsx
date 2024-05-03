6d07f398f613a06405bf02b99bafd8892bc4b132
Renato Silva <renatosilvacode@gmail.com>
2024-05-03 17:35:21 -0300

    refactor: disable GTM and Clarity

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,10 +2,10 @@ import type { Metadata } from 'next'
 
 import { env } from '~/config/env'
 
-import { Clarity } from '~/components/clarity'
+// import { Clarity } from '~/components/clarity'
 import { ConversionBar } from '~/components/conversion-bar'
 import { Footer } from '~/components/footer'
-import { GoogleAnalytics } from '~/components/google-analytics'
+// import { GoogleAnalytics } from '~/components/google-analytics'
 import { Header } from '~/components/header'
 
 // const SaleModal = dynamic(() => import('~/components/sale-modal'), {
@@ -44,8 +44,8 @@ export default function Layout({
 			<div className="flex flex-col flex-1">{children}</div>
 			<Footer />
 			{/* <SaleModal /> */}
-			<GoogleAnalytics />
-			<Clarity />
+			{/* <GoogleAnalytics /> */}
+			{/* <Clarity /> */}
 		</div>
 	)
 }
