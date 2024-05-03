7b63b67ef7efd280bdcd0796de7d11da59af7bc1
Renato Silva <renatosilvacode@gmail.com>
2024-05-03 15:20:16 -0300

    refactor: use dynamic imports on slices

--- src/slices/footer/components.ts
+++
@@ -1,5 +0,0 @@
-import FooterLinks from './FooterLinks'
-
-export const components = {
-	footer_links: FooterLinks
-}
--- src/slices/header/components.ts
+++
@@ -1,5 +0,0 @@
-import MenuItem from './MenuItem'
-
-export const components = {
-	menu_item: MenuItem
-}
